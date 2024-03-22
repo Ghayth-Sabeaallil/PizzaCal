d7a5bf5d3429634bc3a63bff89155952b6b010af
Ghayth Sabeaallil <gs222mh@gmail.com>
2024-03-22 15:40:58 +0100

    animation and border

--- src/Components/PizzaImgDiv/PizzaImgDiv.tsx
+++ src/Components/PizzaImgDiv/PizzaImgDiv.tsx
@@ -11,7 +11,7 @@ const PizzaImgDiv = ({ pizzaDB }: any) => {
             <h2>{pizzaDB[id].namn}</h2>
             <div className="img-arrow">
                 <img onClick={() => setId((count) => (count > 0) ? count - 1 : 0)} src="../assets/img/left.png" alt="" />
-                <img onClick={clickhandle} src={pizzaDB[id].img} width="250" height="250" />
+                <img className="pizza-img" onClick={clickhandle} src={pizzaDB[id].img} width="300" height="300" />
                 <img onClick={() => setId((count) => (count <= 5) ? count + 1 : 6)} src="../assets/img/right.png" alt="" />
             </div>
         </div>
