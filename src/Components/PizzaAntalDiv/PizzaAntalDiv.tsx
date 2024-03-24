df0692aba994877dae03b1b5148b8c9086d923b3
Ghayth Sabeaallil <gs222mh@gmail.com>
2024-03-24 16:20:47 +0100

    add buy btn

--- src/Components/PizzaAntalDiv/PizzaAntalDiv.tsx
+++ src/Components/PizzaAntalDiv/PizzaAntalDiv.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react"
 import "./PizzaAntalDiv.css"
 
 const PizzaAntalDiv = () => {
-    const [antal, setAntal] = useState(0);
     return (
         <>
             <div className="containerAntal">
-                <div unselectable="on" className="decrement" onClick={() => setAntal((antal) => (antal > 0) ? antal - 1 : 0)}>-</div>
-                <p className="antal">{antal}</p>
-                <div unselectable="off" className="increment" onClick={() => setAntal((antal) => antal + 1)}>+</div>
-
+                <div className="cart-btn"><img width="45px" height="45px" src="../assets/img/cart.png" alt="cart" /></div>
             </div>
         </>
     )
