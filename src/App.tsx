78729c68a428db4e93f7405099ed13ca9145bd49
ossegosse <ossian.igelberg@gmail.com>
2024-03-25 09:24:56 +0100

    mergat main

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ import pizzaDB from "../assets/pizza.json"
 import Container from "./Components/ContainerRoot/Container"
 import PizzaContextProvider from "./Components/PizzaContext/PizzaContextProvider"
 import Ingredienser from "./Components/Ingredienser/Ingredienser"
-import ToppingBox from "./Components/Ingredients/IngredientsBox"
-import "./App.css"
 
 function App() {
 
   return (
     <>
-
-      <Card />
+      <PizzaContextProvider>
+        <Container PizzaImgDiv={<PizzaImgDiv pizzaDB={pizzaDB} />} />
+        <Ingredienser />
+      </PizzaContextProvider>
 
     </>
   )
